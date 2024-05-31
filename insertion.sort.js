c10907604aa658cfe6e09a169a79a1911fd71079
mofed_salah <53442697+Mofed14@users.noreply.github.com>
2024-05-31 20:09:17 +0300

    insertion sort function

---
+++ insertion.sort.js
@@ -0,0 +1,16 @@
+function insertionSort() {
+  for (let i = 1; i < unSortedData.length; i++) {
+    const key = unSortedData[i];
+    for (var j = i - 1; j >= 0; j--) {
+      if (unSortedData[j] > key) unSortedData[j + 1] = unSortedData[j];
+      else break;
+    }
+    unSortedData[j + 1] = key;
+  }
+
+  return unSortedData;
+}
+
+const unSortedData = [90, 15, 21, 4, 0, -1];
+
+console.log(insertionSort(unSortedData));
